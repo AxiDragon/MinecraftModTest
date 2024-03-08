3886301e03272259e4c85953a52feb1e6a62ab48
Axi <axidreamdragon@gmail.com>
2024-03-08 15:07:29 +0100

    Create Diamond Amulet item

--- src/main/java/com/axidreamdragon/diamondspawner/DiamondSpawnerMain.java
+++ src/main/java/com/axidreamdragon/diamondspawner/DiamondSpawnerMain.java
@@ -1,10 +1,12 @@
 package com.axidreamdragon.diamondspawner;
 
+import com.axidreamdragon.diamondspawner.init.ItemInit;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -31,6 +33,11 @@ public class DiamondSpawnerMain {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::setup);
+        ItemInit.ITEMS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
