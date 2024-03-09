ab9f246e184aaced599205bd7f902ba8b380a689
Axi <axidreamdragon@gmail.com>
2024-03-09 11:42:39 +0100

    Update DiamondAmulet functionality

--- src/main/java/com/axidreamdragon/diamondspawner/items/DiamondAmulet.java
+++ src/main/java/com/axidreamdragon/diamondspawner/items/DiamondAmulet.java
@@ -1,16 +1,24 @@
 package com.axidreamdragon.diamondspawner.items;
 
+import java.util.Random;
+
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 public class DiamondAmulet extends Item {
+    private float soundPitch = 2.0F;
+    private float velocityRadius = 0.5F;
+    private float upwardVelocity = 1F;
+
     public DiamondAmulet(Properties properties) {
         super(properties);
     }
@@ -19,12 +27,22 @@ public class DiamondAmulet extends Item {
     public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
         ItemStack diamond = new ItemStack(Items.DIAMOND);
 
-        player.getCooldowns().addCooldown(this, 2);
+        ItemEntity diamondEntity = new ItemEntity(world, player.getX(), player.getY() + 0.5, player.getZ(), diamond);
 
-        player.getInventory().add(diamond);
+        diamondEntity.setDeltaMovement(getRandomVelocity());
+
+        diamondEntity.setPickUpDelay(20);
+
+        world.addFreshEntity(diamondEntity);
 
         world.playSound(null, player.getX(), player.getY(), player.getZ(),
-                SoundEvents.EXPERIENCE_ORB_PICKUP, SoundSource.PLAYERS, 1.0F, 3.0F);
+                SoundEvents.EXPERIENCE_ORB_PICKUP, SoundSource.PLAYERS, 1.0F, soundPitch);
+
+        soundPitch += 0.1F;
+
+        if (soundPitch > 2.0F) {
+            soundPitch = 0.5F;
+        }
 
         ItemStack stack = player.getItemInHand(hand);
         stack.setDamageValue(stack.getDamageValue() + 1);
@@ -35,5 +53,17 @@ public class DiamondAmulet extends Item {
 
         return super.use(world, player, hand);
 
+    }
+
+    private Vec3 getRandomVelocity() {
+        Random random = new Random();
+
+        double angle = random.nextDouble() * Math.PI * 2;
+        double distance = velocityRadius * Math.sqrt(random.nextDouble());
+
+        double x = Math.cos(angle) * distance;
+        double z = Math.sin(angle) * distance;
+
+        return new Vec3(x, upwardVelocity, z);
     }
 }
